e3de918f7bc9fe8e57fc12a9203ffe523674e6d3
EdgarGs <eger.2110gs@gmail.com>
2025-08-22 23:48:05 -0600

    Leer DATABASE_URL desde el entorno

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 # database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # URL de conexión a tu base de datos
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:edge211034@localhost:5432/imcufide_db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
